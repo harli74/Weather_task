b920c706cc2a2db2595548e173967a3d5cb94795
Miroslav Cholakov <moltenhardrock@abv.bg>
2022-04-04 00:00:47 +0300

    Random generator implementation
    Fixed the task to work with the default python random generator

--- task1.py
+++ task1.py
@@ -1,46 +1,67 @@
 import requests
-strs=[]
-apilinks=[]
-def locationlist():
-  for i in range(5):
-      strs.append(input())
-locationlist()
-
-def createapilinks():
- for i in range(5):
-   complete_api_link="https://api.openweathermap.org/data/2.5/weather?q="+strs[i]+"&appid=f24acb33b0bcc507760fec5abaa0313a"
-   api_link=requests.get(complete_api_link)
-   apilinks.append(api_link)
-createapilinks()
+import json
+import random
+#strs=[]
+#codes=[]
+randcodes=[]
+#apilinks=[]
+apilinksfromid=[]
+rnd=random.randint(20,100)
+def locationcodes(r):
+    with open("city.list.json") as file:
+         data=json.load(file)
+    for i in range(100000):
+        randcodes.append(random.randint(833,102908597))
+    for i in range(r):
+        if randcodes[i]== data["id"]:
+            codes.append(data["id"])
+locationcodes(rnd)
+def createapilinksfromcodes(r):
+    for i in range(r):
+        complete_api_link="https://api.openweathermap.org/data/2.5/weather?id="+codes[i]+"&appid=f24acb33b0bcc507760fec5abaa0313a"
+        api_link=requests.get(complete_api_link)
+        apilinksfromid.append(api_link)
+createapilinksfromcodes(rnd)
+#def locationlist():
+    #for i in range():
+       # strs.append(input())
+#locationlist()
+#def createapilinks():
+    #for i in range(5):
+        #complete_api_link="https://api.openweathermap.org/data/2.5/weather?q="+strs[i]+"&appid=f24acb33b0bcc507760fec5abaa0313a"
+        #api_link=requests.get(complete_api_link)
+        #apilinks.append(api_link)
+#createapilinks()
 #print (apilinks)
-def average():
-    sum=0
-    avg=0
-    for i in range(5): 
-        api_data=apilinks[i].json()
-        town_temps=((api_data['main']['temp'])-273.15)
-        sum+=town_temps
-        avg=sum/5
-    print ("Average temperature is ",avg)
+def average(r):
+         sum=0
+         avg=0
+         for i in range(r): 
+             api_data=apilinksfromid[i].json()
+             town_temps=((api_data['main']['temp'])-273.15)
+             sum+=town_temps
+             avg=sum/5
+         print ("Average temperature is ",avg)
         
-average()  
-def  coldest():
- mintemp=9000
- coldesttown=" "
- for i in range(5):
-   api_data=apilinks[i].json()
-   town_temps=((api_data['main']['temp'])-273.15)
-   if town_temps<mintemp:mintemp=town_temps
-   if mintemp==town_temps:coldesttown=api_data['name']
- print("Coldest town is:",coldesttown)
+average(rnd)  
+def  coldest(r):
+     mintemp=9000
+     coldesttown=" "
+     for i in range(r):
+         api_data=apilinksfromid[i].json()
+         town_temps=((api_data['main']['temp'])-273.15)
+         if town_temps<mintemp:mintemp=town_temps
+         if mintemp==town_temps:coldesttown=api_data['name']
+     print("Coldest town is:",coldesttown)
   
-coldest()
+coldest(rnd)
 location=input()
 complete_api_link="https://api.openweathermap.org/data/2.5/weather?q="+location+"&appid=f24acb33b0bcc507760fec5abaa0313a"
 api_link=requests.get(complete_api_link)
 api_data=api_link.json()
 #print (api_data)
-if api_data['cod']=='404':print("Wrong city{}",format(location))
+if api_data['cod']=='404':
+    print("Wrong city{}",format(location))
 else:
     town_temps=((api_data['main']['temp'])-273.15) 
     weather=(api_data['weather'][0]['description'])
