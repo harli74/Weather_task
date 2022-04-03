8c60d631ed0706c4b5a1a49dc885332100c52910
Miroslav Cholakov <moltenhardrock@abv.bg>
2022-04-04 01:18:52 +0300

    Function addition
    converted the check for a single city in a function

--- task1.py
+++ task1.py
@@ -36,19 +36,19 @@ def  coldest():
      print("Coldest town is:",coldesttown)
   
 coldest()
-location=input()
-complete_api_link="https://api.openweathermap.org/data/2.5/weather?q="+location+"&appid=f24acb33b0bcc507760fec5abaa0313a"
-api_link=requests.get(complete_api_link)
-api_data=api_link.json()
-if api_data['cod']=='404':
-    print("Wrong city{}",format(location))
-else:
-    town_temps=((api_data['main']['temp'])-273.15) 
-    weather=(api_data['weather'][0]['description'])
-    hmdt=api_data['main']['humidity']
-
-print("Temp: {}".format(town_temps))
-print("Weather Description: ",weather)
-print("Humidity: ",hmdt,'%')
-
+def singletowncheck():
+    location=input()
+    complete_api_link="https://api.openweathermap.org/data/2.5/weather?q="+location+"&appid=f24acb33b0bcc507760fec5abaa0313a"
+    api_link=requests.get(complete_api_link)
+    api_data=api_link.json()
+    if api_data['cod']=='404':
+        print("Wrong city{}",format(location))
+    else:
+        town_temps=((api_data['main']['temp'])-273.15) 
+        weather=(api_data['weather'][0]['description'])
+        hmdt=api_data['main']['humidity']
+        print("Temp: {}".format(town_temps))
+        print("Weather Description: ",weather)
+        print("Humidity: ",hmdt,'%')
+singletowncheck()
 
