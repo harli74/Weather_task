98634f9f594a7b956e1d193ffe07f0f6bdb08a2d
Miroslav Cholakov <moltenhardrock@abv.bg>
2022-04-03 22:15:08 +0300

    Next commit
    Average function is done.Working on coldest.

--- task1.py
+++ task1.py
@@ -1,17 +1,42 @@
 import requests
+strs=[]
+apilinks=[]
+def locationlist():
+  for i in range(5):
+      strs.append(input())
+locationlist()
+
+def createapilinks():
+ for i in range(5):
+   complete_api_link="https://api.openweathermap.org/data/2.5/weather?q="+strs[i]+"&appid=f24acb33b0bcc507760fec5abaa0313a"
+   api_link=requests.get(complete_api_link)
+   apilinks.append(api_link)
+createapilinks()
+print (apilinks)
 def average():
-    strs = [input() for i in range(6)]
-    for i in range(6): 
-        location=strs(i)
-        complete_api_link="https://api.openweathermap.org/data/2.5/weather?q="+location+"&appid=f24acb33b0bcc507760fec5abaa0313a"
-        api_link=requests.get(complete_api_link)
-        api_data=api_link.json()
+    sum=0
+    avg=0
+    for i in range(5): 
+        api_data=apilinks[i].json()
         town_temps=((api_data['main']['temp'])-273.15)
         sum+=town_temps
         avg=sum/5
-        print ("Average temperature is ",avg)
+    print ("Average temperature is ",avg)
         
+average()  
+def  coldest():
+ mintemp=9000
+ coldesttown=" "
+ for i in range(5):
+   api_data=apilinks[i].json()
+   town_temps=((api_data['main']['temp'])-273.15)
+   if town_temps<mintemp:mintemp=town_temps
+   if mintemp==town_temps:api_data
+   
+   
 
+   
+#average()
 location=input()
 complete_api_link="https://api.openweathermap.org/data/2.5/weather?q="+location+"&appid=f24acb33b0bcc507760fec5abaa0313a"
 api_link=requests.get(complete_api_link)
