17805e9d2c3f4fad63090d1c9eb3661c04b21603
Miroslav Cholakov <moltenhardrock@abv.bg>
2022-04-05 01:37:00 +0300

    Task 2 Debugging
    Code practicly works.God 1 problem with the rand function when generating the city ID keys that we must discuss

--- task1.py
+++ task1.py
@@ -2,10 +2,8 @@ import requests
 import random
 from tkinter import *
 from tkinter import ttk
-
 randcodes=[]
 apilinksfromid=[]
-
 def locationcodes():
     global randcodes
     global apilinksfromid
@@ -19,14 +17,15 @@ def locationcodes():
         if api_data['cod']!='404':
             apilinksfromid.append(api_link)
 def average():
-         sum=0
+         tempsum=0
          avg=0
-         for i in range(len(apilinksfromid)): 
+         numberoftowns=len(apilinksfromid)
+         for i in range(numberoftowns): 
              api_data=apilinksfromid[i].json()
              town_temps=((api_data['main']['temp'])-273.15)
-             sum+=town_temps
-             avg=sum/5
-         return avg       
+             tempsum+=town_temps
+             avg=tempsum/numberoftowns     
+         return numberoftowns, avg       
 def  coldest():
      mintemp=9000
      coldesttown=" "
@@ -46,21 +45,21 @@ def singletowncheck(input):
         town_temps=((api_data['main']['temp'])-273.15) 
         weather=(api_data['weather'][0]['description'])
         hmdt=api_data['main']['humidity']
-        return town_temps,weather,hmdt
-        
-        
-#singletowncheck()
-#def averagetogui():
-
+        return town_temps,weather,hmdt                
 def singletownsearch():
     temps,weather,hmdt=singletowncheck(modify.get())
     outputtemp.set("Stats of "+modify.get()+" are :\n Temps:"+str(temps)+"\n Weather: "
                    +weather+"\n Humidity: "+str(hmdt)+" %")
-    
+def avgtogui():
+    numberoftowns,avg=average()
+    outputavg.set("Average temp of "+str(numberoftowns)+" is "+str(avg)+" degrees")
+def coldestgui():
+    outputcoldest.set("Coldest town is "+coldest())
 appwindow=Tk()
 root=appwindow
 outputtemp=StringVar()
 outputavg=StringVar()
+outputcoldest=StringVar()
 appwindow.title("City Checker App")
 appwindow.geometry('640x480')
 lblsearch=Label(appwindow,text='Enter City to Find:').grid(column=0,row=1)
@@ -70,10 +69,12 @@ modify.grid(column=1, row=1)
 btn3=Button(appwindow,text="Check City:",fg="black",command=singletownsearch).grid(column=2,row=1)
 lblrand_cities = Label(appwindow, text="Random Cities:").grid(column=0, row=0)
 btn = Button(appwindow, text="Collect Data!",fg="red",command=locationcodes).grid(column=1, row=0)
-lblaveragevalue = Label(appwindow, text="Random Cities:").grid(column=0, row=3)
+lblaveragevalue = Label(appwindow, text="AverageTemps:").grid(column=0, row=3)
 lblaverageguiresponce=Label(appwindow,textvariable=outputavg).grid(column=2,row=3)
-btn1=Button(appwindow,text="AverageTemps",command=average).grid(column=1,row=3)
-#btn2=Button(appwindow,text="Coldest City:",fg="blue",command=coldest).grid(column=3,row=0)
+btn1=Button(appwindow,text="Generate",command=avgtogui).grid(column=1,row=3)
+lblcoldest = Label(appwindow, text="Find Coldest City:").grid(column=0, row=4)
+lblcoldestguiresponce=Label(appwindow,textvariable=outputcoldest).grid(column=1,row=4)
+btn2=Button(appwindow,text="Coldest City:",fg="blue",command=coldestgui).grid(column=0,row=5)
 btn4=ttk.Button(appwindow, text="Quit", command=root.destroy).grid(column=5, row=5)
 root.mainloop()
 appwindow.mainloop()
