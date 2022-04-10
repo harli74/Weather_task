7bc2e4f428a6eaa668c3b20acaeb3db0a275aae4
MiroslavCholakov <moltenhardrock@abv.bg>
2022-04-10 15:34:41 +0300

    further flask development
    working on the functions.

--- env/task3.py
+++ env/task3.py
@@ -1,8 +1,30 @@
 from flask import Flask
 import random
 import time
+import requests
+import json
 
 app=Flask(__name__)
 @app.route('/')
 def index():
+    url='https://api.openweathermap.org/data/2.5/weather?id={}&units=metric&appid=8bd428589520ff3bfe675be56c83d503'
+
+    req=requests.get(url.format(id)).json()
+
+    resp={ 'city' : req['name'],
+           'temperature' :req['main']['temp'],
+           'humidity' : req['main']['humidity'],
+           'description' : req['weather']['description'],
+        }
     return '<h1>Hello World</h1>'
+
+def single():
+    url='https://api.openweathermap.org/data/2.5/weather?q={}&appid=8bd428589520ff3bfe675be56c83d503'
+
+    req=requests.get(url.format(cityname)).json()
+
+    resp={ 'city' : req['name'],
+           'temperature' :req['main']['temp'],
+           'humidity' : req['main']['humidity'],
+           'description' : req['weather']['description'],
+        }
