e0ff598e2d99934b44a798b593ab6398c4e3182c
MiroslavCholakov <moltenhardrock@abv.bg>
2022-04-11 12:56:34 +0300

    trying to finish the backend

--- env/task3.py
+++ env/task3.py
@@ -10,7 +10,26 @@ app=Flask(__name__)
 @app.route('/')
 def index():
     locationcodes()
-    towntemp,weather,hmdt=singletowncheck()
+    
     townnumbers,avg=average()
     coldtown=coldest()
     return render_template('index.html')
+
+@app.route('/singletown', methods=['GET','POST'])
+def check():
+    if request.method=='POST':
+        towntemp,weather,hmdt=singletowncheck()
+        return '''
+                  <h1>Current temperature is {} degrees </h1>
+                  <h1>Current weather is {} </h1>
+                  <h1>Current humidity is {} % degrees </h1>'''.format(towntemp, weather, hmdt)
+
+@app.route('/coldesttown', methods='POST')
+def coldestt():
+    coldestt=coldest()
+    return '<h1> Coldest town is {} <h1>'.format(coldestt)
+
+@app.route('/calcaverage')
+def calculate():
+    citycount,avg=average()
+    return '<h1> The average temperature of {} cities is {}'.format(citycount,avg)
