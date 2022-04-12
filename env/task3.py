af9530116f81985f87d56366c322eb12808df535
MiroslavCholakov <moltenhardrock@abv.bg>
2022-04-12 19:28:42 +0300

    further torture
    reworking backend to work on the same page.

--- env/task3.py
+++ env/task3.py
@@ -18,21 +18,27 @@ def check():
         if(request.form.get('activate')=='activate'):
             city=request.form.get('srch')
             towntemp,weather,hmdt=singletowncheck(city)
-        return '''
-                  <h1>Current temperature is {} degrees </h1>
-                  <h1>Current weather is {} </h1>
-                  <h1>Current humidity is {} % degrees </h1>'''.format(towntemp, weather, hmdt)
-    return render_template('singletown.html')
+            return '''
+                       <h1>Current temperature is {} degrees </h1>
+                       <h1>Current weather is {} </h1>
+                       <h1>Current humidity is {} % degrees </h1>'''.format(towntemp, weather, hmdt)
+        elif(request.form.get('coldest')=='coldest'):
+            coldestt=coldest()
+            return '<h1> Coldest town is {} <h1>'.format(coldestt)
+        elif(request.form.get('average')=='average'):
+            citycount,avg=average()
+            return '<h1> The average temperature of {} cities is {}'.format(citycount,avg)
+    return render_template('index.html')
 
-@app.route('/coldesttown', methods=['GET', 'POST'])
-def coldestt():
-    coldestt=coldest()
-    return '<h1> Coldest town is {} <h1>'.format(coldestt)
+#@app.route('/coldesttown', methods=['GET', 'POST'])
+#def coldestt():
+   # coldestt=coldest()
+    #return '<h1> Coldest town is {} <h1>'.format(coldestt)
 
-@app.route('/calcaverage')
-def calculate():
-    citycount,avg=average()
-    return '<h1> The average temperature of {} cities is {}'.format(citycount,avg)
+#@app.route('/calcaverage')
+#def calculate():
+    #citycount,avg=average()
+   # return '<h1> The average temperature of {} cities is {}'.format(citycount,avg)
 
 if __name__=="__main__":
     app.run(debug=True)
