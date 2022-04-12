54c05ca4b4c026f44d260ee0e2d7a9287cbca4d4
MiroslavCholakov <moltenhardrock@abv.bg>
2022-04-12 22:43:50 +0300

    working on additional buttons

--- env/task3.py
+++ env/task3.py
@@ -22,23 +22,25 @@ def check():
             data1={"city": city,"towntemp": towntemp,"weather": weather,"hmdt": hmdt}
     
             return render_template('index.html' , data1=data1)
-        #elif(request.form.get('coldest')=='coldest'):
+def calculate():
+    if request.method=='POST':
+        citycount,avg=average()
+        coldestt=coldest()
+        return render_template('index.html', citycount=citycount, avg=avg, cold=coldestt)
+#elif(request.form.get('coldest')=='coldest'):
             #coldestt=coldest()
             #return '<h1> Coldest town is {} <h1>'.format(coldestt)
-        #elif(request.form.get('average')=='average'):
+      #elif(request.form.get('average')=='average'):
             #citycount,avg=average()
             #return '<h1> The average temperature of {} cities is {}'.format(citycount,avg)
     #return render_template('index.html')
 
 #@app.route('/coldesttown', methods=['GET', 'POST'])
-#def coldestt():
-   # coldestt=coldest()
+#
     #return '<h1> Coldest town is {} <h1>'.format(coldestt)
 
 #@app.route('/calcaverage')
-#def calculate():
-    #citycount,avg=average()
-   # return '<h1> The average temperature of {} cities is {}'.format(citycount,avg)
+
 
 if __name__=="__main__":
     app.run(debug=True)
