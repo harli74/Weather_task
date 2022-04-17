5fabae8624a134e8c39499aa789e998171fa2b34
MiroslavCholakov <moltenhardrock@abv.bg>
2022-04-18 00:40:24 +0300

    restarted work on django
    working on django functionality. Stumbled upon unexpected problem.

--- weatherapi/weather/views.py
+++ weatherapi/weather/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
-
+from django.views.decorators.csrf import csrf_exempt
+from .apifuncs import locationcodes,average,coldest,singletowncheck
+@csrf_exempt
 def index(request):
-    return render(request, 'weather/index.html')
+    locationcodes()
+    if(request.POST):
+         city=request.form.get('city')
+         towntemp,weather,hmdt=singletowncheck(city)
+         data1={"city": city,"towntemp": towntemp,"weather": weather,"hmdt": hmdt}
+         citycount,avg=average()
+         coldestt=coldest()
+    return render(request, 'weather/index.html', data1, citycount, avg, coldestt)
