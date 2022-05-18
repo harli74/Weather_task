26011dfb47262dfda6d3e0e0532aef1534031f82
harli74 <milen316@gmail.com>
2022-05-18 16:25:16 +0300

    add comunication betweet django and db

--- task/task_5/views.py
+++ task/task_5/views.py
@@ -12,9 +12,13 @@ apikey="c9a787290254e2833d876e34bbccb790"
 URL=f"https://api.openweathermap.org/data/2.5/weather?"
 key = "q="
 end = "&appid="
+dataLenht =''
+cities_db_info=[]
+inputCount =0
 
 
-
+def refreshcounter(request):
+    dataLenht = Cities.objects.count()
 
 # Create your views here.
 def main_def(request):
@@ -25,32 +29,63 @@ def main_def(request):
     return render(request, 'home.html')
 
 def searchTab(request):
+   
+    dataLenht = Cities.objects.count()
+    print(cities_db_info)
     dataLenht = Cities.objects.count()
     nameData = request.POST.get('Search')
     print(nameData)
-
+    btnRefresh = request.GET.get('btnRefresh')
     btnDelete=request.POST.get('btnDelete')
     
     #print(Cities.objects.get(id=206))
     
     
-    # for x in range(10): 
-    #    Cities.objects.create(city_name='Burgas',humidity=5,temperature=18,weather ='rain')
-    # print(Cities.objects.all().count())
+    
     if nameData != None:
         ApiInputRequest = f"{URL}{key}{nameData}{end}{apikey}&units=metric"
         ApiRequest_Input = requests.get(ApiInputRequest)
         dataweather = ApiRequest_Input.json()
         Main = dataweather['main']
         Weather = dataweather['weather']
+        dataLenht = Cities.objects.count()
+        
+        for x in range(Cities.objects.count()): 
+            shanolud = Cities.objects.get(id=x)
+            refreshcounter(request)
         if Cities.objects.all().count()<10:
-            Cities.objects.create(city_name=nameData,humidity=Main['humidity'],temperature=Main['temp'],weather =Weather[0]['description'])
+            Cities.objects.create(id = Cities.objects.count(),city_name=nameData,humidity=Main['humidity'],temperature=Main['temp'],weather =Weather[0]['description'])
+            
             print(Cities.objects.count())
+            dataLenht = Cities.objects.count()
+            
+            
+            #print(shanolud.city_name,shanolud.weather,shanolud.temperature,shanolud.humidity)
+            dataLenht =0
+            cities_db_info.clear()
+            for x in range(Cities.objects.count()):
+                shanolud= Cities.objects.get(id=x)
+                cities_db_info.append(f"{shanolud.city_name} {shanolud.temperature} {shanolud.humidity} {shanolud.weather}")
+                #append(f"{data['name']} {Weather[0]['description']} {Main['temp']} {Main['humidity']}")
+                dataLenht=+1
+        elif Cities.objects.all().count()>10:
+            print("gg")
+
+    print(dataLenht)
+    refreshcounter(request)
+
             
         #Cities.objects.get(id=0).delete()
     if btnDelete != None:
         print("deleting")
         Cities.objects.all().delete()
+        dataLenht = Cities.objects.count()
+    if(btnRefresh != None):
+
+        print("refresh")
+        refreshcounter(request)
+        dataLenht = Cities.objects.count()
+        
     print("Do something")
     #delete all values of the table
     #
@@ -58,4 +93,4 @@ def searchTab(request):
     #Cities.save()
    # Cities.refresh_from_db()
     
-    return render(request,'searchcity.html',{'input_data':nameData,'db_lenght':dataLenht,})
+    return render(request,'searchcity.html',{'input_data':nameData,'db_lenght':dataLenht,'city_info':cities_db_info})
