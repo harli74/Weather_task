1cab4077d56ee5ad97a3aaa241d39c18895824f7
harli74 <milen316@gmail.com>
2022-05-21 11:55:56 +0300

    task_fixed

--- Task_1/WeatherTask_1.py
+++ Task_1/WeatherTask_1.py
@@ -1,91 +1,73 @@
-# ## [Task 1](#task1). Weather API requests - основи
-
-# Да се напише програма, която:
-
-#   * Да показва какво е времето в момента за 5 произволно избрани града (*бонус точки*: град от цял свят, няма ограничение - освен да е написан на латиница):
-# 	* Дали е облачно/вали или е слънчево ? 
-# 	* Каква е температурата в момента ?
-# 	* Каква е влажността в момента ?
-
-# * Да се изведе следните статистики:
-# 	* Най-студен град (от избраните 5)
-# 	* Средна температура (от избраните 5)
-
-# * **Да може да се въвежда име на град и индивидуално да изкарва статистика:** (бонус точки)
-# 	* Облачно ли е ?
-# 	* Каква е температурата ?
-# 	* Каква е влажността в момента ?
-
-
-# + Нужни неща:
-# 	- Обработване на JSON данни
-# 	- HTTP/HTTPS Requests
-# 	- Random generator
-# 	- User Input
-# 	- http://openweathermap.org/api
-
-#apikey: 07cf9f3accb7195ab333a8b337f932d8
-
-from multiprocessing.sharedctypes import Value
-from numbers import Number
-from operator import index, indexOf
-import string
-from tokenize import Double
-from unicodedata import name
 import requests
 import json
 import random
 
-from youtube_dl import main
-
-print("Start")
 city = []
-cityInfo =[]
-CityTemp =[]
-with open('Resources/city.list.json','r') as f:
-    cityData = json.load(f)
+city_info =[]
+city_temp =[]
+
+apikey="c9a787290254e2833d876e34bbccb790"
+URL=f"https://api.openweathermap.org/data/2.5/weather?"
+key = "q="
+end = "&appid="
+
+class CityGenerate(object):
+
+    def Generate():
+        print("Start")
+
+        with open('Resources/city.list.json','r') as f:
+            cityData = json.load(f)
 
 
-Allcount =0
-for x in range(0,len(cityData)):
-    Allcount+=1
+        Allcount =0
+        for x in range(0,len(cityData)):
+            Allcount+=1
 
-print(Allcount)
+        print(Allcount)
 
-for x in range(5):
-    randomNumber = random.randrange(0,Allcount)
-    city.append(cityData[randomNumber]['name'])
+        for x in range(5):
+            randomNumber = random.randrange(0,Allcount)
+            city.append(cityData[randomNumber]['name'])
 
-    apikey="c9a787290254e2833d876e34bbccb790"
-    URL=f"https://api.openweathermap.org/data/2.5/weather?"
-    key = "q="
-    end = "&appid="
-    apiRequest = URL + key + city[x] + end + apikey + '&units=metric'
-    ApiOutput = requests.get(apiRequest)
+            
+            apiRequest = URL + key + city[x] + end + apikey + '&units=metric'
+            ApiOutput = requests.get(apiRequest)
 
-    data = ApiOutput.json()
-    Main = data['main']
-    Weather = data['weather']
-    cityInfo.append(f"{data['name']} {Weather[0]['description']} {Main['temp']} {Main['humidity']}")
-    CityTemp.append(Main['temp'])
-    print(f"Name of the city: {data['name']}")
-    print(f"Weather Report: {Weather[0]['description']}")
-    print(f"Temperature is: {Main['temp']}")
-    print(f"Humidity is: {Main['humidity']}")
-    print(city)
+            data = ApiOutput.json()
+            Main = data['main']
+            Weather = data['weather']
+            city_info.append(f"{data['name']} {Weather[0]['description']} {Main['temp']} {Main['humidity']}")
+            city_temp.append(Main['temp'])
+            print(f"Name of the city: {data['name']}")
+            print(f"Weather Report: {Weather[0]['description']}")
+            print(f"Temperature is: {Main['temp']}")
+            print(f"Humidity is: {Main['humidity']}")
+            print(city)
 
-print(f"The coldest city is: {cityInfo[CityTemp.index(min(CityTemp))]}")
-AverageTemp = sum(CityTemp) / len(CityTemp)
-print(f"The average Temperature is: {AverageTemp}")
+        print(f"The coldest city is: {city_info[city_temp.index(min(city_temp))]}")
+        AverageTemp = sum(city_temp) / len(city_temp)
+        print(f"The average Temperature is: {AverageTemp}")
+        return object()
 
-InputCityName = input()
-ApiInputRequest = URL + key + InputCityName + end + apikey + '&units=metric'
-ApiRequest_Input = requests.get(ApiInputRequest)
-data = ApiRequest_Input.json()
-Main = data['main']
-Weather = data['weather']
-print(f"Name of the city: {data['name']}")
-print(f"Weather Report: {Weather[0]['description']}")
-print(f"Temperature is: {Main['temp']}")
-print(f"Humidity is: {Main['humidity']}")
 
+
+class InputCity(object):
+    def Input():
+        input_city_name = input()
+        api_input_request = URL + key + input_city_name + end + apikey + '&units=metric'
+        api_request = requests.get(api_input_request)
+        data = api_request.json()
+        main = data['main']
+        weather = data['weather']
+        print(f"Name of the city: {data['name']}")
+        print(f"Weather Report: {weather[0]['description']}")
+        print(f"Temperature is: {main['temp']}")
+        print(f"Humidity is: {main['humidity']}")
+
+
+def Execute():
+    CityGenerate.Generate()
+    InputCity.Input()
+
+Execute()
