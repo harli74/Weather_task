aa7462dce8fd53538098cd00db5180a563b9ebd6
harli74 <milen316@gmail.com>
2022-05-22 19:02:01 +0300

    fix task_5

--- task/task_5/views.py
+++ task/task_5/views.py
@@ -1,28 +1,25 @@
-from ast import Index
-from dataclasses import field
-from operator import index, indexOf
+import weakref
 from django.shortcuts import render
 import requests
 from .models import Cities
 import json
 import random
+
 city = []
-cityInfo =[]
-CityTemp =[]
-Weather=''
-frm = 0
+city_info =[]
+city_temp =[]
 apikey="c9a787290254e2833d876e34bbccb790"
 URL=f"https://api.openweathermap.org/data/2.5/weather?"
 key = "q="
 end = "&appid="
-dataLenht =''
+data_lenght =''
 cities_db_info=[]
-inputCount =0
-shanolud=''
-field1=0
-field2=0
+input_count =0
+city_object=''
+field_1=0
+field_2=0
 def refreshcounter(request):
-    dataLenht = Cities.objects.count()
+    data_lenght = Cities.objects.count()
 
 # Create your views here.
 def main_def(request):
@@ -38,84 +35,84 @@ def main_def(request):
 
 def searchTab(request):
  
-    compareBtn = request.POST.get('compare')
-    field1 = request.POST.get('id_1')
-    field2 = request.POST.get('id_2')
-    dataLenht = Cities.objects.count()
+    compare_btn = request.POST.get('compare')
+    field_1 = request.POST.get('id_1')
+    field_2 = request.POST.get('id_2')
+    data_lenght = Cities.objects.count()
     print(cities_db_info)
-    dataLenht = Cities.objects.count()
-    nameData = request.POST.get('Search')
-    print(nameData)
-    btnRefresh = request.POST.get('btnRefresh')
-    btnDelete=request.POST.get('btnDelete')
+    data_lenght = Cities.objects.count()
+    name_data = request.POST.get('Search')
+    print(name_data)
+    btn_refresh = request.POST.get('btnRefresh')
+    btn_delete=request.POST.get('btnDelete')
     
     #print(Cities.objects.get(id=206))
     
     
     
-    if nameData != None:
-        ApiInputRequest = f"{URL}{key}{nameData}{end}{apikey}&units=metric"
-        ApiRequest_Input = requests.get(ApiInputRequest)
-        dataweather = ApiRequest_Input.json()
-        Main = dataweather['main']
-        Weather = dataweather['weather']
-        dataLenht = Cities.objects.count()
+    if name_data != None:
+        api_input_request = f"{URL}{key}{name_data}{end}{apikey}&units=metric"
+        api_request_input = requests.get(api_input_request)
+        data_weather = api_request_input.json()
+        main = data_weather['main']
+        weather = data_weather['weather']
+        data_lenght = Cities.objects.count()
         
         for x in range(Cities.objects.count()): 
-            shanolud = Cities.objects.get(id=x)
+            city_object = Cities.objects.get(id=x)
             refreshcounter(request)
         if Cities.objects.all().count()<10:
-            Cities.objects.create(id = Cities.objects.count(),city_name=nameData,humidity=Main['humidity'],temperature=Main['temp'],weather =Weather[0]['description'])
+            Cities.objects.create(id = Cities.objects.count(),city_name=name_data,humidity=main['humidity'],temperature=main['temp'],weather =main[0]['description'])
             
             print(Cities.objects.count())
-            dataLenht = Cities.objects.count()
+            data_lenght = Cities.objects.count()
             
             
-            #print(shanolud.city_name,shanolud.weather,shanolud.temperature,shanolud.humidity)
-            dataLenht =0
+            #print(city_object.city_name,city_object.weather,city_object.temperature,city_object.humidity)
+            data_lenght =0
             
             for x in range(Cities.objects.count()):
-                shanolud= Cities.objects.get(id=x)
+                city_object= Cities.objects.get(id=x)
                
                 #append(f"{data['name']} {Weather[0]['description']} {Main['temp']} {Main['humidity']}")
-                dataLenht=+1
+                data_lenght=+1
         elif Cities.objects.all().count()>10:
             print("gg")
 
-    print(dataLenht)
+    print(data_lenght)
     refreshcounter(request)
 
             
         #Cities.objects.get(id=0).delete()
-    if btnDelete != None:
+    if btn_delete != None:
         print("deleting")
         Cities.objects.all().delete()
-        dataLenht = Cities.objects.count()
-    if(btnRefresh != None):
+        data_lenght = Cities.objects.count()
+    if(btn_refresh != None):
         
         print("refresh")
         refreshcounter(request)
         Cities.objects.all().order_by('temperature')
-        dataLenht = Cities.objects.count()
+        data_lenght = Cities.objects.count()
         cities_db_info.clear()
         for x in range(Cities.objects.count()):
-                shanolud= Cities.objects.get(id=x)
-                cities_db_info.append(f"{shanolud.city_name} {shanolud.temperature} {shanolud.humidity} {shanolud.weather}")
+                city_object= Cities.objects.get(id=x)
+                cities_db_info.append(f"{city_object.city_name} {city_object.temperature} {city_object.humidity} {city_object.weather}")
                 #append(f"{data['name']} {Weather[0]['description']} {Main['temp']} {Main['humidity']}")
-                dataLenht=+1
+                data_lenght=+1
         
         
-        return render(request,'searchcity.html',{'input_data':nameData,'db_lenght':dataLenht,'city_info':cities_db_info})
+        return render(request,'searchcity.html',{'input_data':name_data,'db_lenght':data_lenght,'city_info':cities_db_info})
         
-    if compareBtn != None:
-        if field1 != None and field2 != None:
+    if compare_btn != None:
+        if field_1 != None and field_2 != None:
            
             print("compare")
           
-            shanolud1=Cities.objects.get(id=field1)
-            shanolud2=Cities.objects.get(id=field2)
-            city_1_info = f"{shanolud1.city_name} {shanolud1.temperature} {shanolud1.humidity} {shanolud1.weather}"
-            city_2_info = f"{shanolud2.city_name} {shanolud1.temperature} {shanolud1.humidity} {shanolud1.weather}"
+            city_object1=Cities.objects.get(id=field_1)
+            city_object2=Cities.objects.get(id=field_2)
+            city_1_info = f"{city_object1.city_name} {city_object1.temperature} {city_object1.humidity} {city_object1.weather}"
+            city_2_info = f"{city_object2.city_name} {city_object2.temperature} {city_object2.humidity} {city_object2.weather}"
           
             return render(request,'searchcity.html',{'field1':city_1_info,'field2':city_2_info,})
     print("Do something")
@@ -125,6 +122,6 @@ def searchTab(request):
     #Cities.save()
    # Cities.refresh_from_db()
    
-    print(field1)
-    print(field2)
-    return render(request,'searchcity.html',{'input_data':nameData,'db_lenght':dataLenht,'city_info':cities_db_info,})
+    print(field_1)
+    print(field_2)
+    return render(request,'searchcity.html',{'input_data':name_data,'db_lenght':data_lenght,'city_info':cities_db_info,})
